48aea6d490b766794058bc702c66f61ed8d51586
yfedi <yfedi@softserveinc.com>
2020-06-04 17:47:38 +0300

    Initial commit for Turtlebot Docker Image

---
+++ docker/ros2_turtlebot/run.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR_PATH=$(dirname "$SCRIPT_PATH")
+WS_DIR_PATH=$(realpath "$SCRIPT_DIR_PATH/../../../..")
+
+xhost +local:root
+
+docker run -it --rm \
+    --privileged \
+    --volume=/tmp/.X11-unix:/tmp/.X11-unix \
+    --volume="$WS_DIR_PATH:/root/ws" \
+    --device=/dev/dri:/dev/dri \
+    --env="DISPLAY=$DISPLAY" \
+    --env QT_X11_NO_MITSHM=1 \
+    --add-host simulation:178.18.0.2 \
+    --add-host turtlebot_1:178.18.0.6 \
+    --hostname turtlebot_0 \
+    --net ros_net \
+    --ip 178.18.0.3 \
+   ros2_gazebo_8_ws
+
+
+xhost -local:root
